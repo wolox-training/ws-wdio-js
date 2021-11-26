a600c4fce3d1a002c7afe3934adf0f290ed6d5fb
Wendy Sierra <wendy.sierra@wolox.co>
2021-11-26 08:32:31 -0500

    implementation automation for login

--- tests/stepsDefinitions/LoginSteps.js
+++ tests/stepsDefinitions/LoginSteps.js
@@ -6,12 +6,12 @@ Given(/^I am in the login screen/, () => {
   LoginScreen.waitForIsShown();
   LoginScreen.tabLogin();
 });
-When(/^I login with the user data "(.*)"/, user => {
-  LoginScreen.login(global.users[user].email,global.users[user].password)
-});
 
+When(/^I login with the user data "(.*)"/, user => {
+  LoginScreen.login(global.users[user].email, global.users[user].password)
+});
 
 When(/^I login with the incorrect data (.*) and (.*)/, (email, password) => {
   LoginScreen.login(email, password);
-  
+
 });
